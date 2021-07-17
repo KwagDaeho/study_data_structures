0543c13ac7fa9f340b38bfe8d1a7277da974617d
user <user@userui-MacBookPro.local>
2021-07-17 16:30:21 +0900

    STUDY start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>THIS IS DATA STRUCTURES.</h1>
       </header>
+      <ol>
+        <li><a href="list/array.js">array</a></li>
+        <li><a href="">queue</a></li>
+        <li><a href="">stack</a></li>
+        <li><a href="">linked list</a></li>
+        <li><a href="">tree</a></li>
+        <li><a href="">binary tree</a></li>
+      </ol>
     </div>
   );
 }
